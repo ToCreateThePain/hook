69013664999f04418c09f53c4a90515fe98a45ad
ToCreateThePain <125130279+ToCreateThePain@users.noreply.github.com>
2023-02-11 16:56:44 +0200

    Update start.py

--- start.py
+++ start.py
@@ -2,9 +2,6 @@ import os
 import json
 from flask import Flask, request, Response
 import telebot
-
-now_utc = [datetime.now().utcnow(), "No date"]
-results_main = []
 bot = telebot.TeleBot(os.environ.get("TOKEN"))
 app = Flask(__name__)
 
@@ -18,32 +15,8 @@ def handle():
         send_main()
 
 
-def send_main():
-    for i in range(3):
-        try:
-            if len(results_main) != 0:
-                image = results_main.pop()
-                bot.send_photo(
-                    chat_id="@mjrecent_f",
-                    photo=image["link"],
-                    caption=image["prompt"],
-                    parse_mode="Markdown"
-                )
-            else:
-                bot.send_message(os.environ.get("LOGS_USERNAME"), "No images, called get_main(now_utc)")
-                get_main(now_utc)
-                continue
-
-            break
-        except Exception as e:
-            e = str(e)
-            bot.send_message(os.environ.get("LOGS_USERNAME"), f"error:\n{e}")
-            if "Bad Request" in e:
-                continue
-            else:
-                break
-                
-    bot.send_message(os.environ.get("LOGS_USERNAME"), f"{len(results_main)} pictures left.")
+def sender():
+    
 
 
 app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 3000)))
