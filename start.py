0066b41a59e8a5d40021d2931c0f24d407544ce2
ToCreateThePain <125130279+ToCreateThePain@users.noreply.github.com>
2023-02-11 16:56:04 +0200

    Update start.py

--- start.py
+++ start.py
@@ -1,6 +1,5 @@
 import os
 import json
-from datetime import datetime
 from flask import Flask, request, Response
 import telebot
 
@@ -17,38 +16,6 @@ def handle():
 
     elif request.method == 'HEAD':
         send_main()
-
-
-def get_main(prev_utc):
-    bot.send_message(os.environ.get("LOGS_USERNAME"), f"Before get_main time is\n{str(prev_utc)}")
-    results_main.clear()
-    response = requests.get("https://www.midjourney.com/showcase/recent/")
-    soup = BeautifulSoup(response.text, 'html.parser')
-    scripts = soup.find_all("script")
-    for script in scripts:
-        if script.get('id') != None:
-            data = json.loads(script.text)
-            jobs = data['props']['pageProps']['jobs']
-            if prev_utc[1] == "No date":
-                now_utc[1] = "Set date"
-                for job in jobs:
-                    results_main.append({"link": job['image_paths'][0],
-                                         "prompt": f"`{job['full_command']}`"
-                                         })
-            else:
-                for job in jobs:
-                    day_job = int(job['enqueue_time'][8:10])
-                    hour_job = int(job['enqueue_time'][11:13])
-                    min_job = int(job['enqueue_time'][14:16])
-                    if prev_utc[0].day <= day_job and prev_utc[0].hour <= hour_job and prev_utc[0].minute <= min_job:
-                        results_main.append({"link": job['image_paths'][0],
-                                             "prompt": f"`{job['full_command']}`"
-                                             })
-
-            break
-
-    now_utc[0] = datetime.now().utcnow()
-    bot.send_message(os.environ.get("LOGS_USERNAME"), f"Got {len(results_main)} new images!\n{now_utc[1]} {str(now_utc[0])}")
 
 
 def send_main():
