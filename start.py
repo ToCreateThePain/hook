32d3b54417f24c128d0f8eedc1187d352d9382ca
ToCreateThePain <125130279+ToCreateThePain@users.noreply.github.com>
2023-02-11 17:10:08 +0200

    Update start.py

--- start.py
+++ start.py
@@ -1,20 +1,15 @@
 import os
-import json
-from flask import Flask, request
+from flask import Flask, request, Response
 import telebot
 bot = telebot.TeleBot(os.environ.get("TOKEN"))
 app = Flask(__name__)
-
 
 @app.route('/', methods=['POST'])
 def handle():
     if request.form.get("mypas") != None:
         sender(request.form.get("mypas"))
-
-        
+    return Response("Got u", 123)  
 
 def sender(obj):
     bot.send_photo(os.environ.get("DIR"), obj['link'], caption=obj['text'], parse_mode="Markdown")
-    
-
 app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 3000)))
